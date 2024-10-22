2bed3dbb5976414353fbf9ccc5d0779c2864758b
FranciscoGarcia <francisco.garcia@trainibit.com>
2024-10-22 14:05:11 -0600

    First commit

---
+++ src/main/java/mx/gob/metepec/government_employees/GovernmentEmployeesApplication.java
@@ -0,0 +1,13 @@
+package mx.gob.metepec.government_employees;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GovernmentEmployeesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GovernmentEmployeesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mx/gob/metepec/government_employees/GovernmentEmployeesApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.gob.metepec.government_employees;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GovernmentEmployeesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
